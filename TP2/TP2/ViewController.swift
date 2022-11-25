475fa51c0074ac8b0b50c2371fb3e89b4d200c77
kazimar007 <kazimar.terraria@gmail.com>
2022-11-25 08:03:03 -0800

    Debut TP2

---
+++ TP2/TP2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TP2
+//
+//  Created by user228865 on 11/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
