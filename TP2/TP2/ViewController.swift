eb3854cd783db7da75fe029f8e730e95b761a05a
kazimar007 <kazimar.terraria@gmail.com>
2022-12-09 05:43:19 -0800

    FrontEnd termine

--- TP2/TP2/ViewController.swift
+++ TP2/TP2/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labWordToFind: UILabel!
+    @IBOutlet weak var txfLetterEntry: UITextField!
+    @IBOutlet weak var labAllLetter1: UILabel!
+    @IBOutlet weak var labAllLetter2: UILabel!
+    @IBOutlet weak var imgBody: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
