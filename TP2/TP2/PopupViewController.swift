3cad1958c3674196cd2a40cf1c4efc7b8df839d4
kazimar007 <kazimar.terraria@gmail.com>
2022-12-15 19:45:03 -0800

    le table view est fonctionnel de base

--- TP2/TP2/PopupViewController.swift
+++ TP2/TP2/PopupViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+var names = [
+    "asd", "asdas"]
 
 class PopupViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func btnQuitterPressed(_ sender: Any) {
         dismiss(animated: true)
+    }
+}
+
+extension PopupViewController: UITableViewDelegate{}
+
+extension PopupViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
     }
 }
