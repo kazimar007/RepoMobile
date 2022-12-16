a94eed1d636c6f3ca27bce5880fe1ffec6a0eb9a
kazimar007 <kazimar.terraria@gmail.com>
2022-12-15 17:37:50 -0800

    Le popup pour le highscore a ete creer

---
+++ TP2/TP2/PopupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PopupViewController.swift
+//  TP2
+//
+//  Created by user228865 on 12/15/22.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func btnQuitterPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
